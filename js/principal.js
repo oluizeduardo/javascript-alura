12ae72be947cb956827cd9b789372a240f9d6138
Luiz Eduardo Costa <costaeduardoluiz@gmail.com>
2018-06-29 12:01:52 -0300

    Adding patient.

--- js/principal.js
+++ js/principal.js
@@ -18,6 +18,56 @@ for(var i=0; i < pacientes.length; i++){
 
   if(imc < 20 || imc > 28){
     paciente.classList.add("paciente-invalido");
-    console.log(paciente);
   }
 }
+
+// Seleciona o botão.
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+// Adiciona uma função de click no botão.
+botaoAdicionar.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTR = document.createElement("tr");
+
+  var nomeTD = document.createElement("td");
+  var pesoTD = document.createElement("td");
+  var alturaTD = document.createElement("td");
+  var gorduraTD = document.createElement("td");
+  var icmTD = document.createElement("td");
+
+  nomeTD.textContent = nome;
+  pesoTD.textContent = peso;
+  alturaTD.textContent = altura;
+  gorduraTD.textContent = gordura;
+  icmTD.textContent = 0;
+
+  pacienteTR.appendChild(nomeTD);
+  pacienteTR.appendChild(pesoTD);
+  pacienteTR.appendChild(alturaTD);
+  pacienteTR.appendChild(gorduraTD);
+  pacienteTR.appendChild(icmTD);
+
+  var tabela = document.querySelector("#tabela-paciente");
+  tabela.appendChild(pacienteTR);
+
+});
+
+
+// // Função anônima.
+// titulo.addEventListener("click", function(){
+//   alert("Click captado!!");
+// });
+
+// Chamada de função por parâmetro.
+// titulo.addEventListener("click", mostrarMensagem);
+//
+// function mostrarMensagem(){
+//   alert("Click captado!!");
+// }
